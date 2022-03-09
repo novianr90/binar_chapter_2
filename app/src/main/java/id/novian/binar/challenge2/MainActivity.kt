3205319fe04fc2ec91d3d211965580ea2d85d4ad
Novian Rachman Iskandar <novian19@upi.edu>
2022-03-10 05:10:58 +0700

    changes grid and add bottom top

--- app/src/main/java/id/novian/binar/challenge2/MainActivity.kt
+++ app/src/main/java/id/novian/binar/challenge2/MainActivity.kt
@@ -1,11 +1,23 @@
 package id.novian.binar.challenge2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
+        openActivityGojek()
     }
+
+    private fun openActivityGojek() {
+        val buttonGojek = findViewById<Button>(R.id.buttonGojek)
+        buttonGojek.setOnClickListener {
+            val intent = Intent(this, GojekActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
